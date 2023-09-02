6cbc5ee4fa272bf5d5013ba9b8f15bb8619fbb20
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2023-09-02 19:51:59 +0100

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://adelinked.github.io/edie-homepage/",
   plugins: [react()],
 });
